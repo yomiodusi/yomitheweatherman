a1bf3a36c0e956764da87b6eed432b7c7a2971c1
yomiodusi <99558274+yomiodusi@users.noreply.github.com>
2022-04-01 01:14:59 -0400

    updated html and started loop for 5days 0114am

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
-class Fetch {
-    
-}
+var previousSearch = []
+
+
+// Api call
+$("#submit").click(function () {
+  //Get value from when user types in city
+  var input = $("#userInput").val();
+  previousSearch.push (input)
+  console.log(previousSearch)
+  $.getJSON(
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+      input +
+      "&units=metric&APPID=a05a45a9905cbe052cad2b21f009f6bb",
+    function (data) {
+        $("#temp").text(data.main.temp)
+        $("#wind").text(data.wind.speed)
+        $("#humi").text(data.main.humidity)
+        $("#uvi").text(data.main.temp)
+        console.log(data)
+    }
+  );
+});
+
+// A $( document ).ready() block.
+$(document).ready(function () {
+  var currentDate = moment();
+  $("#cityPlace").text("Atlanta " + currentDate.format("ll"));
+  for (let index = 1; index < 6; index++) {
+    var futureDays = moment().add(index, "days");
+    $("#day" + index).text(futureDays.format("ll"));
+  }
+});
